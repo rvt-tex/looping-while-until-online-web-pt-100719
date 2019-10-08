2591c286dcf783d8281d5874220283551ea6fe1b
Ruel Tingling <rvtingling@yahoo.com>
2019-10-08 08:41:54 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,10 @@
 def using_while
   levitation_force = 6
   counter = 0
-while counter < 10
-  puts "The current number is less than 20."
+while levitation_force < 10
+  puts "Wingardium Leviosa"
   counter += 1
 end
-	
-  #your code here
 end
 
 
